3921c773f8fdde6920b6aff19d90667fed396624
pbv <dev@pb.vision>
2024-01-27 12:11:00 -0800

    export makeService for use with unit tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { runService } from './main.js'
+import { makeService, runService } from './main.js'
 import { enqueueCloudTask } from './tasks.js'
 import * as utils from './utils.js'
 
 export {
   enqueueCloudTask,
+  makeService, // for use with unit testing
   runService,
   utils
 }
