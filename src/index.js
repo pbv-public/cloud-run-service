6f2bdb0435c2c5f798e1a880e2e56b57e6b8106f
pbv <dev@pb.vision>
2024-03-02 12:04:00 -0800

    export getServiceProtocolAndHost

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { makeService, runService } from './main.js'
 import { enqueueCloudTask } from './tasks.js'
-import { isCloud, isDev, isProd, isLocalhost, isUnitTesting } from './utils.js'
+import { isCloud, isDev, isProd, isLocalhost, isUnitTesting, getServiceProtocolAndHost } from './utils.js'
 
 export {
   enqueueCloudTask,
   makeService, // for use with unit testing
   runService,
 
+  getServiceProtocolAndHost,
   isCloud, isDev, isProd, isLocalhost, isUnitTesting
 }
